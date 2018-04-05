3a60f16e1e0f25aff66f5891b4bfe48ba928715b
Brian Peters <BrianRPeters@gmail.com>
2018-04-05 14:20:30 -0500

    Add files via upload

---
+++ Bandwidth.ps1
@@ -0,0 +1,19 @@
+﻿while ($true)
+{
+# Get an object for the network interfaces, excluding any that are currently disabled.
+$colInterfaces = Get-WmiObject -class Win32_PerfFormattedData_Tcpip_NetworkInterface |select BytesTotalPersec, CurrentBandwidth,PacketsPersec|where {$_.PacketsPersec -gt 0}   
+
+   foreach ($interface in $colInterfaces) {
+      $totalBytes = $interface.BytesTotalPersec
+      $totalKiloBytes = $totalBytes / 1000
+      $totalMegaBytes = $totalKiloBytes / 1024
+
+      #if ($totalBits -gt 0) {
+         cls
+         Write-Host $totalBytes " B/s"
+         Write-Host $totalKiloBytes " KB/s"
+         Write-Host $totalMegaBytes " MB/s"
+         Start-Sleep -m 250
+      #}
+   }
+}
